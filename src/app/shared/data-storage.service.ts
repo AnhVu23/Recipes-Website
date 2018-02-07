1890141e9f32421e6f1599b9c41bbc817433f33b
Anh Vu <vu.haianh291@gmail.com>
2018-02-08 00:24:55 +0200

    Update query params in data-storage service

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,6 +1,6 @@
 import { Recipe } from './../recipes/recipe.model';
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
 import { RecipeService } from '../recipes/recipe.service';
 import { Response } from '@angular/http';
 import {AuthService} from "../auth/auth.service";
@@ -13,18 +13,19 @@ export class DataStorageService {
 
   storeRecipes() {
     const token = this.authService.getToken();
-    const settings = {
-      headers: new HttpHeaders().set('Content-Type', 'application/json')
-    };
-    return this.http.put('https://recipe-book-angular-f8505.firebaseio.com/recipes.json?auth=' + token, this.recipeService.getRecipes(),
-      settings);
+    return this.http.put('https://recipe-book-angular-f8505.firebaseio.com/recipes.json', this.recipeService.getRecipes(),
+      {
+        observe: 'body',
+        params: new HttpParams().set('auth', token)
+      });
   }
 
   getRecipes() {
     const token = this.authService.getToken();
-    return this.http.get('https://recipe-book-angular-f8505.firebaseio.com/recipes.json?auth=' + token, {
+    return this.http.get('https://recipe-book-angular-f8505.firebaseio.com/recipes.json', {
       observe: 'body',
-      responseType: 'json'
+      responseType: 'json',
+      params: new HttpParams().set('auth', token);
     });
   }
 }
